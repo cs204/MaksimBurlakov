4236eedd81f94218efb8610de3a3c68dd6fca90b
test <test>
2023-10-07 00:25:22 +0700

    test

---
+++ bank.py
@@ -0,0 +1,9 @@
+name = input ("Приветствие:")
+if name=="здравствуйте":
+        print ("$0")
+elif name=="здравствуйте, человек":
+        print ("$0")
+elif name=="здрасте":
+        print (" $20")
+else:
+        print ("$100")
